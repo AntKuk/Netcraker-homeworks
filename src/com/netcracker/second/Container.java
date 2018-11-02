6a50a2bb7cddb371bae5d3cc3f44cd22042aa6c8
AntKuk <antkuk@hotmail.com>
2018-11-02 12:14:32 +0300

    Overridden equals() and hashCode() in Container class

--- src/com/netcracker/second/Container.java
+++ src/com/netcracker/second/Container.java
@@ -58,4 +58,34 @@ public class Container {
         String str = new String("Container[(" + x1 + "," + y1 + "),(" + x2 + "," + y2 + ")]");
         return str;
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj == null) {
+            return false;
+        }
+        if(this == obj) {
+            return true;
+        }
+        if(!(obj instanceof Container)) {
+            return false;
+        }
+
+        Container container = (Container) obj;
+
+        boolean result = container.x1 == this.x2 & container.y1 == this.y1 & container.x2 == this.x2 & container.y2 == this.y2;
+        return result;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 17;
+
+        result = 31 * result + x1;
+        result = 31 * result + y1;
+        result = 31 * result + x2;
+        result = 31 * result + y2;
+
+        return result;
+    }
 }
